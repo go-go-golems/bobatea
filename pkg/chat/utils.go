edf063d09e631ac88b237428e87957cd4087830c
Manuel Odendahl <wesen@ruinwesen.com>
2024-01-05 20:16:57 +0100

    :tractor: Move chat UI to the bobatea package

---
+++ pkg/chat/utils.go
@@ -0,0 +1,17 @@
+package chat
+
+import (
+	"fmt"
+	"github.com/muesli/reflow/wordwrap"
+)
+
+func wrapWords(text string, w int) string {
+	w_ := wordwrap.NewWriter(w)
+	_, err := fmt.Fprint(w_, text)
+	if err != nil {
+		panic(err)
+	}
+	_ = w_.Close()
+	v := w_.String()
+	return v
+}
