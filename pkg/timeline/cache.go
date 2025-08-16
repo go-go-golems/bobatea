bbe626320527f41e9b4b9fb7e313364201f543eb
Manuel Odendahl <wesen@ruinwesen.com>
2025-08-16 18:57:06 -0400

    :fire: Remove empty cache/renderers

--- pkg/timeline/cache.go
+++
@@ -1,3 +0,0 @@
-package timeline
-
-// Cache removed with stateless renderers; interactive models render directly.
--- pkg/timeline/renderers.go
+++
@@ -1,3 +0,0 @@
-package timeline
-
-// Stateless renderers removed; Bubble Tea models are the only rendering path now.
